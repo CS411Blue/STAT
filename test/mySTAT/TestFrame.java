951ffc50b9e353b5381525e3deadedb01ca82ccb
Brian_2 <Brian_2@128.82.9.38>
2013-11-23 18:45:11 -0500

    Stuff

--- test/mySTAT/TestFrame.java
+++ test/mySTAT/TestFrame.java
@@ -26,120 +26,38 @@ import java.math.*;
 import java.util.Iterator;
 import javax.swing.border.Border;
 import javax.swing.border.LineBorder;
+
+
 /**
  * @author Brian_2
  */
-@SuppressWarnings("serial")
-class RelTest extends JPanel
-{   
-    private final mxGraph panGraph = new mxGraph();
-    private mxIGraphLayout layout;
-    mxGraphComponent graphComponent;
-    private Object parent;
-    
-    RelTest()
-    {
-        super(new BorderLayout());
-        setSize(500, 500);
-        init();
-    }
-    
-    final private void init()
-    {
-        graphComponent = new mxGraphComponent(panGraph);
-        add(BorderLayout.CENTER, graphComponent);
-        parent = panGraph.getDefaultParent();
-        layout = new mxFastOrganicLayout(panGraph);
-    }
-    
-    public void updateShVertexList(ArrayList<Stakeholder> SHList)
-    {
-        //remove all stakeholders currently graphed
-        panGraph.removeCells(panGraph.getChildVertices(panGraph.getDefaultParent()), true);
-        
-        //add all stakeholders to the graph first, no edges
-        for(Iterator<Stakeholder> iter = SHList.iterator(); iter.hasNext();)
-        {
-            Stakeholder s = iter.next();
-            Object v = new Object();
-            if(s.getPlacementRank()>Stakeholder.UNDEFINED)
-            {
-                v = panGraph.insertVertex(parent, null, s.getName(), 100, 100, 
-                        s.getDiameter(), s.getDiameter(), s.getStyle());
-                s.setGraphNode(v);
-            }
-            else
-            {
-                v = panGraph.insertVertex(parent, null, null, 100, 100, s.getDiameter(), s.getDiameter(), s.getStyle());
-                s.setGraphNode(v);
-            }
-        }
-        //now add all edges
-        for(Iterator<Stakeholder> mainShIter = SHList.iterator(); mainShIter.hasNext();)
-        {
-            Stakeholder main = mainShIter.next();
-            for (Iterator<Relationship> it = main.getInfluences().iterator(); it.hasNext();) 
-            {
-                Relationship r = it.next();
-                for (Iterator<Stakeholder> secondaryShIter = SHList.iterator(); secondaryShIter.hasNext();) 
-                {
-                    Stakeholder secondary = secondaryShIter.next();
-                    if(secondary.getName() == r.getId()) 
-                    {
-                        System.out.printf("insertEdge(%s,%s,%s)\n", main.getName(), secondary.getName(), r.getLineStyle());
-                        panGraph.insertEdge(parent, null, null, 
-                                main.getGraphNode(), secondary.getGraphNode(), r.getLineStyle());
-                    }
-                }
-            }
-        }
-        morphLayout();
-    }
-    
-    private void morphLayout()
-    {
-        panGraph.getModel().beginUpdate();
-        try {
-            layout.execute(parent);
-        } finally {
-            mxMorphing morph = new mxMorphing(graphComponent, 20, 1.2, 20);
-
-            morph.addListener(mxEvent.DONE, new mxEventSource.mxIEventListener() {
-                public void invoke(Object arg0, mxEventObject arg1) {
-                    panGraph.getModel().endUpdate();
-                }
-            });
-
-            morph.startAnimation();
-        }
-    }
-    
-    public void graph()
-    {
-        morphLayout();
-    }
-}
-
-
-/*==================================================================*/
-
 
 @SuppressWarnings("serial")
 public class TestFrame extends JFrame
 {
-    private static JPanel panel;
-    private static RelTest testPanel = new RelTest();
-    private static ArrayList<Stakeholder> stakeholders = new ArrayList<Stakeholder>();
-    private static JButton makeSHButton = new JButton("Make Stakeholder");
-    private static JButton pushAllSHButton = new JButton("Push to View");
-    private static JButton makeRelationsButton = new JButton("Initialize Relations for All Stakeholders");
-    private static JPanel makeEdgePanel = new JPanel();
+    private JPanel panel;
+    private RelationMapPanel testPanel = new RelationMapPanel();
+    private ArrayList<Stakeholder> stakeholders = new ArrayList<Stakeholder>();
+    private JButton makeSHButton = new JButton("Make Stakeholder");
+    private JButton pushAllSHButton = new JButton("Push to View");
+    private JButton makeRelationsButton = new JButton("Initialize Relations for All Stakeholders");
+    private JPanel makeEdgePanel = new JPanel();
     
     public static void main(String[] args)
     {
         TestFrame t = new TestFrame();
-        //Create RelTest JPanel and JFrame
-        
+        t.setVisible(true);
+    }
+    
+    public TestFrame()
+    {
+        super("Control Window");
+        setLayout(new BorderLayout());
+        setSize(400,300);
+        setLocation(500,10);
+        setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        panel = new JPanel();
+        getContentPane().add(panel, BorderLayout.NORTH);
         makeSHButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 makeSHButtonActionPerformed(e);
@@ -177,28 +95,16 @@ public class TestFrame extends JFrame
         frame.getContentPane().add(testPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
-        t.setVisible(true);
-    }
-    
-    public TestFrame()
-    {
-        super("Control Window");
-        setLayout(new BorderLayout());
-        setSize(400,300);
-        setLocation(500,10);
-        setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-        panel = new JPanel();
-        getContentPane().add(panel, BorderLayout.NORTH);
     }
 
-    public static void graphButtonActionPerformed(ActionEvent evt)
+    public void graphButtonActionPerformed(ActionEvent evt)
     {
         testPanel.graph();
     }
     
-    public static void makeSHButtonActionPerformed(ActionEvent evt)
+    public void makeSHButtonActionPerformed(ActionEvent evt)
     {
-        String name = "Stakeholder " + (stakeholders.size());
+        String name = "Stakeholder " + (this.stakeholders.size());
         boolean a,b,c,d,e;
         a=b=c=d=e=false;
         int random = (int) (Math.random()*32);
@@ -214,12 +120,12 @@ public class TestFrame extends JFrame
         pushAllSHButton.setText("Push " + stakeholders.size()+" to View");
     }
     
-    public static void pushAllSHButtonActionPerformed(ActionEvent evt)
+    public void pushAllSHButtonActionPerformed(ActionEvent evt)
     {
         testPanel.updateShVertexList(stakeholders);
     }
     
-    public static void makeRelationsButtonActionPerformed(ActionEvent evt)
+    public void makeRelationsButtonActionPerformed(ActionEvent evt)
     {
         System.out.println("In make relationships");
         int k = stakeholders.size();
