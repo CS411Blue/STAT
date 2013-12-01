09d41703d77dff5f71e4fb585ca27242ac9e5eec
Brian_2 <Brian_2@10.252.80.191>
2013-12-01 02:14:14 -0500

    this is to test to see how me and chris' updates to the git ignore handle the creation of new classes that we would like to add to everyone's project. Will be reverted.

---
+++ src/mySTAT/MyDumbNewClass.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mySTAT;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Brian_2
+ */
+public class MyDumbNewClass extends JFrame {
+    public MyDumbNewClass()
+    {
+        super();
+        System.out.println("You're dumb");
+    }
+}
